808b46ee094b5b8ee6df761e09127ea6e920de0b
klhrt <54511837+klhrt@users.noreply.github.com>
2024-07-05 13:27:17 -0400

    Update kylesetup.sh

--- kylesetup.sh
+++ kylesetup.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 sudo apt update
 sudo apt install -y fish compton polybar feh firefox polybar xinit autorandr kitty pcmanfm redshift libudev-dev alsa libasound2-dev build-essential pkg-config
-sudo snap install --beta nvim --classic
 
 sudo cp -rd .config/ ~/
 sudo cp -rd Pictures/ ~/
 sudo cp -rd fonts/ /usr/share/
 sudo cp ./shells /etc/shells
 
-/usr/lib/apt/apt-helper download-file https://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2023.02.18_all.deb keyring.deb SHA256:a511ac5f10cd811f8a4ca44d665f2fa1add7a9f09bef238cdfad8461f5239cc4
-sudo apt install -y ./keyring.deb
-echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" | sudo tee /etc/apt/sources.list.d/sur5r-i3.list
-sudo apt update
-sudo apt install -y i3
+sudo apt install -y awesome
 
 cd ~/Downloads
 wget -c http://archive.ubuntu.com/ubuntu/pool/main/l/lvm2/liblvm2app2.2_2.02.176-4.1ubuntu3.18.04.3_amd64.deb
@@ -28,6 +23,8 @@ chsh -s /usr/bin/fish
 
 cargo install porsmo
 cargo install ttyper
+cargo install ncspot
+cargo install helix
 sudo apt update
 sudo apt upgrade -y
 
